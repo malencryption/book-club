2861d1d9c2bf4cb2c00b4bbcf2b07cc8d9b2c2f6
sunshinedesign36 <runn3rgirl36@gmail.com>
2014-07-07 20:02:06 -0600

    add authorlookup and test data

--- book_club.sql
+++ book_club.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 07, 2014 at 10:58 PM
+-- Generation Time: Jul 08, 2014 at 03:59 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -32,8 +32,16 @@ CREATE TABLE IF NOT EXISTS `account` (
   `password` varchar(50) NOT NULL,
   `userId` int(10) unsigned NOT NULL,
   PRIMARY KEY (`accountId`),
+  UNIQUE KEY `email` (`email`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`accountId`, `email`, `password`, `userId`) VALUES
+(1, 'test@test.com', 'testuser', 1);
 
 -- --------------------------------------------------------
 
@@ -45,10 +53,39 @@ CREATE TABLE IF NOT EXISTS `author` (
   `authorId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
-  `authorBookId` int(11) NOT NULL,
-  PRIMARY KEY (`authorId`),
-  KEY `authorBookId` (`authorBookId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`authorId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`authorId`, `firstName`, `lastName`) VALUES
+(1, 'J.K.', 'Rowling'),
+(2, 'Dan', 'Brown');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authorlookup`
+--
+
+CREATE TABLE IF NOT EXISTS `authorlookup` (
+  `lookupId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `bookId` int(10) unsigned NOT NULL,
+  `authorId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`lookupId`),
+  KEY `bookId` (`bookId`,`authorId`),
+  KEY `authorId` (`authorId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `authorlookup`
+--
+
+INSERT INTO `authorlookup` (`lookupId`, `bookId`, `authorId`) VALUES
+(1, 1, 1),
+(2, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -59,12 +96,18 @@ CREATE TABLE IF NOT EXISTS `author` (
 CREATE TABLE IF NOT EXISTS `book` (
   `bookId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL,
-  `authorId` int(10) unsigned NOT NULL,
   `genreId` int(10) unsigned NOT NULL,
   PRIMARY KEY (`bookId`),
-  KEY `authorId` (`authorId`),
   KEY `genreId` (`genreId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`bookId`, `title`, `genreId`) VALUES
+(1, 'Harry Potter and the Sorcerer''s Stone', 1),
+(2, 'The Da Vinci Code', 2);
 
 -- --------------------------------------------------------
 
@@ -73,13 +116,20 @@ CREATE TABLE IF NOT EXISTS `book` (
 --
 
 CREATE TABLE IF NOT EXISTS `club` (
-  `groupId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `clubId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `dateCreated` date NOT NULL,
   `bookId` int(10) unsigned NOT NULL,
   `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`groupId`),
+  PRIMARY KEY (`clubId`),
   KEY `bookId` (`bookId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `club`
+--
+
+INSERT INTO `club` (`clubId`, `dateCreated`, `bookId`, `name`) VALUES
+(1, '2014-07-07', 1, 'Harry Potter 1');
 
 -- --------------------------------------------------------
 
@@ -88,11 +138,21 @@ CREATE TABLE IF NOT EXISTS `club` (
 --
 
 CREATE TABLE IF NOT EXISTS `clubmember` (
-  `memberId` int(10) unsigned NOT NULL,
+  `memberId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `clubId` int(10) unsigned NOT NULL,
   `accountId` int(10) unsigned NOT NULL,
-  KEY `memberId` (`memberId`,`clubId`,`accountId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`memberId`),
+  KEY `memberId` (`memberId`,`clubId`,`accountId`),
+  KEY `clubId` (`clubId`),
+  KEY `accountId` (`accountId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `clubmember`
+--
+
+INSERT INTO `clubmember` (`memberId`, `clubId`, `accountId`) VALUES
+(1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -104,10 +164,19 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `commentId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `content` varchar(300) NOT NULL,
   `date` date NOT NULL,
-  `memberId` int(10) unsigned NOT NULL,
+  `accountId` int(10) unsigned NOT NULL,
+  `postId` int(10) unsigned NOT NULL,
   PRIMARY KEY (`commentId`),
-  KEY `memberId` (`memberId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `memberId` (`accountId`),
+  KEY `postId` (`postId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`commentId`, `content`, `date`, `accountId`, `postId`) VALUES
+(1, 'your post is a test', '2014-07-07', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -119,7 +188,15 @@ CREATE TABLE IF NOT EXISTS `genre` (
   `genreId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`genreId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`genreId`, `name`) VALUES
+(1, 'fantasy'),
+(2, 'mystery');
 
 -- --------------------------------------------------------
 
@@ -132,10 +209,17 @@ CREATE TABLE IF NOT EXISTS `post` (
   `title` varchar(50) NOT NULL,
   `content` varchar(200) NOT NULL,
   `date` date NOT NULL,
-  `memberId` int(10) unsigned NOT NULL,
+  `accountId` int(10) unsigned NOT NULL,
   PRIMARY KEY (`postId`),
-  KEY `memberId` (`memberId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `memberId` (`accountId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`postId`, `title`, `content`, `date`, `accountId`) VALUES
+(1, 'test post', 'this is a post ', '2014-07-07', 1);
 
 -- --------------------------------------------------------
 
@@ -148,7 +232,63 @@ CREATE TABLE IF NOT EXISTS `user` (
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userId`, `firstName`, `lastName`) VALUES
+(1, 'test', 'user');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`);
+
+--
+-- Constraints for table `authorlookup`
+--
+ALTER TABLE `authorlookup`
+  ADD CONSTRAINT `authorlookup_ibfk_2` FOREIGN KEY (`authorId`) REFERENCES `author` (`authorId`),
+  ADD CONSTRAINT `authorlookup_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookId`);
+
+--
+-- Constraints for table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`genreId`) REFERENCES `genre` (`genreId`);
+
+--
+-- Constraints for table `club`
+--
+ALTER TABLE `club`
+  ADD CONSTRAINT `club_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookId`);
+
+--
+-- Constraints for table `clubmember`
+--
+ALTER TABLE `clubmember`
+  ADD CONSTRAINT `clubmember_ibfk_1` FOREIGN KEY (`accountId`) REFERENCES `account` (`accountId`),
+  ADD CONSTRAINT `clubmember_ibfk_2` FOREIGN KEY (`clubId`) REFERENCES `club` (`clubId`);
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `post` (`postId`),
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`accountId`) REFERENCES `account` (`accountId`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`accountId`) REFERENCES `account` (`accountId`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
